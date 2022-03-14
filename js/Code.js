e7e9ae0ec517d60230a0e9ebaf3098c1f336aa0b
Krepartgames <90005643+Krepartgames@users.noreply.github.com>
2022-03-14 16:47:14 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,12 @@
+const accessCode1 = "variavel";
+const accessCode2 = "funcao";
+const accessCode3 = "banco de dados";
+
+function clues() {
+  textSize(15);
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+  text("Dica: Você pode sempre chama-lo", 700, 170);
+  text("Dica: Não aceita dinheiro, mas aceita informações", 100, 300);
+  // Adicione código para exibir as outras duas perguntas.
+}
